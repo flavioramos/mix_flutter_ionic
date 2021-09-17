b60019580dd6a9ec1ae1462aa77654e35f489e63
Flavio Augusto Silva Ramos <flavio.augusto.ramos@accenture.com>
2021-09-17 15:41:26 -0300

    implementado chamada para activity ionic a partir do flutter

--- android/app/src/main/kotlin/com/example/flutter_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_project/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.example.flutter_project
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.ionic.starter.MainActivity
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        GeneratedPluginRegistrant.registerWith(flutterEngine!!)
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, "com.example.ecs_flutter_test/test")
+                .setMethodCallHandler { call, result ->
+                    when (call.method) {
+                        "openIonic1" -> {
+                            startActivity(Intent(context, MainActivity::class.java))
+                        }
+                    }
+                }
+    }
 }
